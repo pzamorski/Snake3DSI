5edb5d80be9e96f5fb68bf6cfd38f3dc808d46a9
pzamorski <p.zamorski89@gmail.com>
2024-04-08 20:50:25 +0200

    08-04

---
+++ src/test/java/org/patzam/NetworkManagerTest.java
@@ -0,0 +1,51 @@
+package org.patzam;
+
+import junit.framework.TestCase;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.rl4j.learning.configuration.QLearningConfiguration;
+import org.deeplearning4j.rl4j.learning.sync.qlearning.discrete.QLearningDiscreteDense;
+import org.deeplearning4j.rl4j.network.dqn.DQNFactoryStdDense;
+import org.patzam.network.GameContext;
+import org.patzam.network.NetworkManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class NetworkManagerTest extends TestCase {
+
+    public void testBuildConfig() {
+        QLearningConfiguration config = NetworkManager.buildConfig();
+        assertNotNull(config);
+    }
+
+    public void testBuildDQNFactory() {
+        DQNFactoryStdDense dqnFactory = NetworkManager.buildDQNFactory();
+        assertNotNull(dqnFactory);
+    }
+
+    public void testLoadNetwork() {
+        MultiLayerNetwork network = NetworkManager.loadNetwork("models/model.zip");
+        assertNotNull(network); // Assuming the file doesn't exist for testing purposes
+    }
+
+    public void testWaitMicroseconds() {
+        try {
+            Method waitMicrosecondsMethod = NetworkManager.class.getDeclaredMethod("waitMicroseconds", long.class);
+            waitMicrosecondsMethod.setAccessible(true);
+            waitMicrosecondsMethod.invoke(null, 1000L);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            fail("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    public void testSave() {
+        QLearningDiscreteDense<GameContext> dql = null; // Assuming it's initialized somewhere in the test
+        try {
+            Method saveMethod = NetworkManager.class.getDeclaredMethod("save", QLearningDiscreteDense.class, String.class);
+            saveMethod.setAccessible(true);
+            saveMethod.invoke(null, dql, "models/model.zip");
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            fail("Exception occurred: " + e.getMessage());
+        }
+    }
+}
